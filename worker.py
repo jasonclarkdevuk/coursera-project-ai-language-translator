9ec223aea22031f6e3a18a1726f9ada9c5a0e983
jasonclarkdevuk <jasonclarkdev@gmail.com>
2026-05-25 13:27:06 -0400

    Updated worker functions for STT and TTS using Watsonx APIs

--- worker.py
+++ worker.py
@@ -40,11 +40,59 @@ model = Model(
     project_id=PROJECT_ID
 )
 
+# Audio data is received and passed to IBM Watsonx STT APIX
 def speech_to_text(audio_binary):
-    return None
+    base_url = "https://sn-watson-stt.labs.skills.network"
+    api_url = base_url + "/speech-to-text/api/v1/recognize"
+
+    # Set up parameters for HTTP request
+    # Set language and acoustic model framework to US English
+    # Expecting multimedia like audio / video tracks
+    parameters = {
+        "model": "en_US_Multimedia"
+    }
+
+    # Send HTTP POST request
+    # Data body of audio binary data
+    response = requests.post(
+        api_url, params=parameters, data=audio_binary
+    ).json()
+
+    text = "null"
+
+    # Check if 'results' exists and is not empty
+    if response.get("results"):
+        # Safely grab the last result and its last alternative transcript
+        latest_result = response.get("results")[-1]
+        text = latest_result.get("alternatives")[-1].get("transcript")
+
+    return text
 
 def text_to_speech(text, voice=""):
-    return None
+    # Set up Watson TTS HTTP API URL
+    base_url = "https://sn-watson-tts.labs.skills.network"
+    api_url = base_url + '/text-to-speech/api/v1/synthesize?output=output_text.wav'
+
+    # If user has specified a voice, add parameter to API url
+    if voice != "" and voice != "default":
+        api_url += "&voice=" + voice
+    
+    # Set headers for request
+    headers = {
+        "Accept": "audio/wav", # Send audio data in WAV format
+        "Content-Type": "application/json" # Format of body of POST request is JSON        
+    }
+
+    # Set up request body
+    json_data = {
+        "text": text
+    }
+
+    # Send HTTP POST request   
+    # Response is binary audio data in the content of response
+    response = requests.post(api_url, headers=headers, json=json_data)
+
+    return response.content
 
 # Prompt is passed to Watsonx's Mistral API to a receive a response
 def watsonx_process_message(user_message):
